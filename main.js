7374a830d091d0f4c82086c3d19c61deb708a13b
kimyongkwon <http://github.com/kimyongkwon/01_web_project.git>
2020-07-24 00:07:34 +0900

    head section
    - animation
    - navbar modify

--- main.js
+++ main.js
@@ -8,6 +8,14 @@
 const head = document.querySelector("#head");
 document.addEventListener("scroll", () => {
   if (window.scrollY > 400) {
+    head.classList.remove("appear");
     head.classList.add("disappear");
+    console.log(window.scrollY);
+  }
+
+  if (window.scrollY < 400) {
+    head.classList.remove("disappear");
+    head.classList.add("appear");
+    console.log(window.scrollY);
   }
 });
