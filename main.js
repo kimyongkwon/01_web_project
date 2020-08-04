ac3235367edf07b45259bc70430f5ba4ff791924
kimyongkwon <http://github.com/kimyongkwon/01_web_project.git>
2020-08-05 00:15:45 +0900

    index, other html
    - index part
    - other html part

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 "use_district";
 
-// const navbar = document.querySelector(".navbar__menu__item");
-// navbar.addEventListener("click", () => {
-
-// });
-
-const headerScrollY = 350;
-
+const selected = document.querySelector(".selected");
 const head = document.querySelector("#head");
 const header = document.querySelector(".header__content");
-document.addEventListener("scroll", () => {
-  if (window.scrollY > headerScrollY) {
-    head.classList.remove("appear");
-    head.classList.add("disappear");
 
-    // header.classList.remove("head__disappear");
-    header.classList.add("header__appear");
-    header.classList.remove("header__disappear");
-    // console.log(window.scrollY);
-  }
+if (selected.dataset.link == "MASSIVELY") {
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > 350) {
+      head.classList.remove("appear");
+      head.classList.add("disappear");
 
-  if (window.scrollY < headerScrollY) {
-    head.classList.remove("disappear");
-    head.classList.add("appear");
+      // header.classList.remove("head__disappear");
+      header.classList.add("header__appear");
+      header.classList.remove("header__disappear");
+      // console.log(window.scrollY);
+    }
 
-    header.classList.remove("header__appear");
-    header.classList.add("header__disappear");
-  }
-});
+    if (window.scrollY < 350) {
+      head.classList.remove("disappear");
+      head.classList.add("appear");
+
+      header.classList.remove("header__appear");
+      header.classList.add("header__disappear");
+    }
+  });
+} else {
+  const headTitle = document.querySelector(".head__title");
+  const headDesc = document.querySelector(".head__description");
+  const headArrow = document.querySelector(".head__arrow");
+  // head.classList.add("invisible");
+  headTitle.classList.add("invisible");
+  headDesc.classList.add("invisible");
+  headArrow.classList.add("invisible");
+  header.classList.add("header__more__appear");
+}
